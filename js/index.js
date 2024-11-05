d4671cc22099dfbe98983e9a38d348d381791aef
Yaroslav Musiychuk <65865743+Yarik353@users.noreply.github.com>
2024-11-05 13:12:15 +0300

    1

--- js/index.js
+++ js/index.js
@@ -145,7 +145,7 @@ function handleStart() {
         setTimeout(() => {
             document.querySelector('.copyButton.'+nn).innerHTML =`COPY`  
         }, 1000);
-        navigator.clipboard.writeText('kFH8XdnZKXfKCRL5cTSJRY1dkntdp1BDZWnpYU5pump');
+        navigator.clipboard.writeText('DUokPtvG6UKTCGRAUn7o7GWjNbc7zhckwe4mE2ypump');
 
     }
     
